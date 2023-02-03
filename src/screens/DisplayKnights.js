229741492da4865d47f728884d7a31e37bcb9daa
CCeciliaDev <ceciliadev@outlook.com>
2023-02-03 09:48:58 +0100

    knightsDetails

---
+++ src/screens/DisplayKnights.js
@@ -0,0 +1,21 @@
+import './DisplayKnights.css';
+
+
+const DisplayKnights = () => {
+  return (
+    <div className='mainDivDisplayKnights'>
+      <div className='divNameImg'>
+		    <p>images</p>
+		    <p>firstname</p>
+      </div>
+      <div className='divDescEtc'>
+        <p>origine</p>
+        <p>power%</p>
+        <p>Quests</p>
+        <p>description</p>
+    </div>
+	</div>
+  );
+}
+
+export default DisplayKnights;
