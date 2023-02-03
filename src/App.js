f5e9e6e6572972db021cd177c584493c06aebe3b
Helene <helene.blanche@hotmail.fr>
2023-02-03 11:13:12 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
-import "./App.css"
-import { Routes, Route } from "react-router-dom"
-import Header from "./components/Header"
-import Quests from "./screens/Quests"
-import DisplayQuests from "./screens/DisplayQuests"
-import Knights from "./screens/Knights"
-import DisplayKnights from "./screens/DisplayKnights"
-import Team from "./screens/Team"
-import Home from "./screens/Home"
+import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Quests from './screens/Quests'
+import DisplayQuests from './screens/DisplayQuests'
+import Knights from './screens/Knights'
+import DisplayKnights from './screens/DisplayKnights'
+import Team from './screens/Team'
+import Home from './screens/Home'
+import QuestCard from './components/QuestCard'
+import ScreenQuest from './screens/ScreenQuest'
 
 function App() {
   return (
-    <div className="App">
-			<Header />
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/quests" element={<Quests />} />
-        <Route path="/quests/:id" element={<DisplayQuests />} />
-        <Route path="/knights" element={<Knights />} />
-				<Route path="/knights/:id" element={<DisplayKnights />} />
-        <Route path="/team" element={<Team />} />
-			</Routes>
-		</div>
-  );
+    <div className='App'>
+      <Header />
+      <QuestCard />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/quests' element={<Quests />} />
+        <Route path='/quests/:id' element={<DisplayQuests />} />
+        <Route path='/knights' element={<Knights />} />
+        <Route path='/knights/:id' element={<DisplayKnights />} />
+        <Route path='/screenquest' element={<ScreenQuest />} />
+        <Route path='/team' element={<Team />} />
+      </Routes>
+    </div>
+  )
 }
 
-export default App;
+export default App
